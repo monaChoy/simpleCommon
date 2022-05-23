9b772c7d8c1bc00de83fc8175f8708867f63b842
caiwenxiao <zywc2012@hotmail.com>
2022-05-23 13:50:42 +0800

    基础代码提交

---
+++ pc-common-common/src/main/java/com/pccommon/CommonApplication.java
@@ -0,0 +1,16 @@
+package com.pccommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class}
+)
+public class CommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+
+}
