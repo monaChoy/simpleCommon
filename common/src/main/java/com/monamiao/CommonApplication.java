3794ff44200a9e5612a9740de832fb3a5bbbd4b1
caiwenxiao <zywc2012@hotmail.com>
2022-06-02 13:33:22 +0800

    修改包名

--- common/src/main/java/com/monamiao/CommonApplication.java
+++ common/src/main/java/com/monamiao/CommonApplication.java
@@ -1,4 +1,4 @@
-package com.pccommon;
+package com.monamiao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- common/src/test/java/com/monamiao/CommonApplicationTests.java
+++ common/src/test/java/com/monamiao/CommonApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pccommon;
+package com.monamiao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
